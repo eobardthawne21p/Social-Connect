82073eed50a95345406ca7f0435aa6ccb453d8c4
Paul Hitchcox <142123149+eobardthawne21p@users.noreply.github.com>
2024-09-15 23:34:09 +0000

    added step definitions

---
+++ features/step_definitions/social_connect_steps.rb
@@ -0,0 +1,95 @@
+Given('There are two logged-in users, Jack and Jill') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given('I sign in as Jack') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given('I view a post') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I click the {string} button') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('the post will be added to my saved posts') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given('There is a user, Jack') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given('I see the login button') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I sign in as Jack') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I am able to see the timeline') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given('I can view posts on the timeline') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given('I select a post') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I should see the chatboard in a new window') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I should see maps to the event in a new window') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given('I see a button {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I will see a pop-up with options for both maps and the chatboard for the post') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given('I am viewing the timeline') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I should see my saved posts in a new window') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given('There is a user Jack') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I view the timeline') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I should see buttons for {string}, {string} and {string}') do |string, string2, string3|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given('There are two users with posts, Jack and Jill') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given('I visit the homepage') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I should see posts on the timeline') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I should see them in reverse-order') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
