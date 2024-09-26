61a3c213d24387c2adbdebaea098e2ec2e8ca9ec
Francisco Javier Cortez <francisco.cortez01@utrgv.edu>
2024-09-26 10:45:46 -0500

    Added scaffold for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   resources :users, except: [ :new ] # This removes the new action
 
   get "sign_up", to: "users#new", as: "sign_up"
@@ -17,5 +18,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: "users#new"
 end
