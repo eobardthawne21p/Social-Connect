c95fe2684fff92d95ce4f0b53b4032ccbb206602
Francisco Javier Cortez <francisco.cortez01@utrgv.edu>
2024-10-01 12:45:41 -0500

    Fixed CI tests for database

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "database_cleaner/mongoid"
 
 module ActiveSupport
   class TestCase
@@ -8,5 +9,15 @@ module ActiveSupport
     parallelize(workers: :number_of_processors)
 
     # Add more helper methods to be used by all tests here...
+     # Setup DatabaseCleaner
+     DatabaseCleaner.strategy = :deletion # Use deletion for Mongoid
+
+     setup do
+       DatabaseCleaner.start
+     end
+ 
+     teardown do
+       DatabaseCleaner.clean
+     end
   end
 end
