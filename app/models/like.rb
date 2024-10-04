eee926bbdcbe48b6bbaae4bbf3855d0243d3ff24
Paul Hitchcox <142123149+eobardthawne21p@users.noreply.github.com>
2024-10-04 05:51:05 +0000

    got rid of likes

--- app/models/like.rb
+++
@@ -1,6 +0,0 @@
-class Like
-  include Mongoid::Document
-  include Mongoid::Timestamps
-  field :post_id, type: String
-  field :user_id, type: String
-end
--- test/factories/likes.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-    factory :like do
-      post_id { "MyString" }
-      user_id { "MyString" }
-    end
-  end
