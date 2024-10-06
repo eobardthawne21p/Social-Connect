b970c8a63a9df43003a55f7b49f3d96d9892c468
Francisco Javier Cortez <francisco.cortez01@utrgv.edu>
2024-10-05 19:08:25 -0500

    Added login authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,25 @@ class SessionsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.find_by(username: params[:username])
+  
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "Logged in successfully."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Invalid username or password."
+      render :new
+    end
+  end
+  
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out successfully."
+    redirect_to login_path
+  end
+
 end
