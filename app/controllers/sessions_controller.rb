926c7f8806487c610d67b9e3ee7fdf2de1cc41c5
Francisco Javier Cortez <francisco.cortez01@utrgv.edu>
2024-10-05 19:18:47 -0500

    Fixed sign up errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:username])
-  
+
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       flash[:notice] = "Logged in successfully."
@@ -15,12 +15,11 @@ class SessionsController < ApplicationController
       render :new
     end
   end
-  
+
 
   def destroy
     session[:user_id] = nil
     flash[:notice] = "Logged out successfully."
     redirect_to login_path
   end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,12 +23,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      session[:user_id] = @user.id
-  
-      redirect_to root_path, notice: "Account created successfully."
-    else
-      render :new
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to posts_path, notice: "User was successfully created." }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
