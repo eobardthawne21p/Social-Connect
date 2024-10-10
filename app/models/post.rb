9c2f55782958bbf0bfa41d20fe06910daf70a65e
Paul Hitchcox <eobardthawne21p@gmail.com>
2024-10-09 23:38:51 -0500

    chnaged string to image

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post
   include Mongoid::Timestamps
   field :title, type: String
   field :description, type: String
-  field :image, type: String
+  field :image, type: Image
   field :location, type: String
   field :timeDate, type: Time
   field :likes, type: Integer
