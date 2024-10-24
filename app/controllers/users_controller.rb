7bc4499911b720af3f4c41948755e6ba587e9db1
Francisco Javier Cortez <francisco.cortez01@utrgv.edu>
2024-10-24 14:25:43 -0500

    Lint Fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        session[:user_id] = @user.id 
+        session[:user_id] = @user.id
         format.html { redirect_to root_path, notice: "Account was successfully created!" }
         format.json { render :show, status: :created, location: @user }
       else
