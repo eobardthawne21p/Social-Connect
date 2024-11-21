34a4647c2139626b128f6e58d4425da0ccd7bea5
Paul Hitchcox <eobardthawne21p@gmail.com>
2024-11-21 17:05:18 -0600

    lint

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -1,16 +1,14 @@
 require "test_helper"
 
 class ProfilesControllerTest < ActionDispatch::IntegrationTest
-
   setup do
     @user = User.create!(name: "Test User", username: "testuser", password: "Password123@", password_confirmation: "Password123@", birthday: "2000-01-01")
   end
 
   test "should get show" do
-    
-    sign_in_as(@user) 
+    sign_in_as(@user)
 
-    
+
     get profile_url(@user)
     assert_response :success
   end
