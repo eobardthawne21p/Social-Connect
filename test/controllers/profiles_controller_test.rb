46b256af52fcd90bcff65dafa9d7ca5c8a4b588b
Paul Hitchcox <eobardthawne21p@gmail.com>
2024-11-21 17:04:25 -0600

    added necessary fields to test

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class ProfilesControllerTest < ActionDispatch::IntegrationTest
 
   setup do
-    @user = User.create!(name: "Test User", username: "testuser", password: "Password123@")
+    @user = User.create!(name: "Test User", username: "testuser", password: "Password123@", password_confirmation: "Password123@", birthday: "2000-01-01")
   end
 
   test "should get show" do
