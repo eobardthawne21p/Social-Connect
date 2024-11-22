1cddfe753bec7991c7bb2d7f63885e479d47891f
Francisco Javier Cortez <francisco.cortez01@utrgv.edu>
2024-11-22 14:43:16 -0600

    Added change password feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
+    @user = User.where(username: params[:username]).first
 
-    if @user && @user.authenticate(params[:password])
+    if @user&.authenticate(params[:password])
       session[:user_id] = @user.id
       flash[:notice] = "Logged in successfully."
       redirect_to root_path
@@ -15,7 +15,6 @@ class SessionsController < ApplicationController
       render :new
     end
   end
-
 
   def destroy
     session[:user_id] = nil
