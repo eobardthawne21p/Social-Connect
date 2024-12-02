c754153e0828550e7d57e141e9b4f5d2f24c4e1c
Paul Hitchcox <eobardthawne21p@gmail.com>
2024-12-01 23:16:55 -0600

    fixed rails-helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,3 @@
-Capybara.javascript_driver = :selenium_chrome_headless
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
