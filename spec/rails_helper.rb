e37fd365399674c394348bbb782e9426a7991d6c
Francisco Javier Cortez <francisco.cortez01@utrgv.edu>
2024-12-09 11:35:40 -0600

    Database cleaner for spec tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,39 +1,38 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-# # At the top of the file
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'database_cleaner-mongoid'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
-
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
-# Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
 
 RSpec.configure do |config|
   # Remove this line to enable support for ActiveRecord
   config.use_active_record = false
 
-  # If you enable ActiveRecord support you should uncomment these lines,
-  # note if you'd prefer not to run each example within a transaction, you
-  # should set use_transactional_fixtures to false.
-  #
-  # config.fixture_path = Rails.root.join('spec/fixtures')
-  # config.use_transactional_fixtures = true
+  # Configure DatabaseCleaner for MongoDB
+  begin
+    DatabaseCleaner.strategy = :deletion
+  rescue NameError
+    raise "You need to add 'database_cleaner-mongoid' to your Gemfile (in the :test group) if you wish to use it."
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:deletion) # Ensures the database is clean before running the test suite
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :deletion # Use deletion strategy for MongoDB
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -54,4 +53,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Uncomment to run tests in random order and debug seed-based failures
+  config.order = :random
+  Kernel.srand config.seed
 end
